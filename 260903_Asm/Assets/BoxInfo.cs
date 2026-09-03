0fcd26ea336ac05377737ca0d761c178221722d8
Jay <jayy5288@gmail.com>
2026-09-03 17:44:06 +0900

    [Feat] 2단계 중간

---
+++ 260903_Asm/Assets/BoxInfo.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxInfo : MonoBehaviour
+{
+    [SerializeField] private string _boxName;
+
+    private void Start()
+    {
+        Debug.Log($"Start: {_boxName}가 벨트에 올라왔습니다.");
+    }
+
+    private void OnDestroy()
+    {
+        Debug.Log($"OnDestroy: {_boxName}가 벨트 끝으로 내려갔습니다.");
+    }
+}
